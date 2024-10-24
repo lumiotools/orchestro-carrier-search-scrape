d83542c4c1b0ef0a1e387ebe7daa1e8a99134ea2
ritesh <rieshdhapatepatil@gmail.com>
2024-10-24 15:21:16 +0530

    extracting the data from excel sheet

--- extract_sheet1_data.py
+++ extract_sheet1_data.py
@@ -2,4 +2,14 @@ import pandas as pd
 
 
 def extractSheet1Data():
-    return
+    # Read the Excel file
+    df = pd.read_excel('data/sheet1.xlsx')
+    
+    # Extract URLs from the second column
+    df = df[321].dropna()
+    urls=[]
+    for data in df:
+         print(data)
+         urls.append(data)
+    return urls
+
